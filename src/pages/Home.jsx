6d8e31287d0e4b842708d9a9c098f85ca12ed400
raph2ll <raphael.ph2ll@gmail.com>
2022-10-30 19:43:01 -0300

    feat(home): create home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Nav from '../components/Nav';
+import Card from '../components/Card';
 
 function Home() {
+  const [products, setProducts] = useState([]);
+  const url = 'http://localhost:3000/products';
+
+  const getProducts = async () => {
+    try {
+      const { data } = await axios.get(url);
+      setProducts(data);
+      return products;
+    } catch (err) {
+      return err;
+    }
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, [setProducts]);
+
   return (
-    <div>Test</div>
+    <div className="flex flex-col min-h-screen">
+      <Nav />
+      <div className="flex justify-center flex-row flex-wrap">
+        {
+          products.map((product) => <Card key={product.id} data={product} />)
+        }
+      </div>
+    </div>
   );
 }
 
