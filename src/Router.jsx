4c01497de1da656f8718a3d1f89995c1e558b8b1
raph2ll <raphael.ph2ll@gmail.com>
2022-10-30 19:43:49 -0300

    build(config): create css config

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import React from 'react';
 import Home from './pages/Home';
+import CreateProduct from './pages/CreateProduct';
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/product" element={<CreateProduct />} />
     </Routes>
   );
 }
