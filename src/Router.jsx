095f445479ca60da53b441a02037c7d835ca61c2
raph2ll <raphael.ph2ll@gmail.com>
2022-10-31 18:40:28 -0300

    feat(Router): create a new Route to edit

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 import React from 'react';
 import Home from './pages/Home';
 import CreateProduct from './pages/CreateProduct';
+import EditProduct from './pages/EditProduct';
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/product" element={<CreateProduct />} />
+      <Route path="/product/:id" element={<EditProduct />} />
     </Routes>
   );
 }
